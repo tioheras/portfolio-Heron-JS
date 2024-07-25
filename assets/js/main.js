3b4674553ddccebf5f768f05237b459f533e5e29
tioheras <eng.heroncarli@gmail.com>
2024-07-25 15:51:00 -0300

    adicionando

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,18 @@
 
 function updateProfileInfo(profileData) {
+    const photo = document.getElementById('profile.photo');
+    photo.src = profileData.photo;
+    photo.alt = profileData.name;
 
-    const photo = document.getElementById('profile.photo')
-    photo.src = profileData.photo
-    photo.alt = profileData.name
-
+    document.getElementById('profile.name').textContent = profileData.name;
+    document.getElementById('profile.job').textContent = profileData.job;
+    document.getElementById('profile.location').textContent = profileData.location;
+    document.getElementById('profile.telefone').textContent = profileData.telefone;
+    document.getElementById('profile.email').textContent = profileData.email;
+    document.getElementById('profile.email').href = `mailto:${profileData.email}`;
 }
 
 (async () => {
-
-    const profileData = await fetchProfileData()
-    updateProfiledate
-})()
+    const profileData = await fetchProfileData();
+    updateProfileInfo(profileData);
+})();
