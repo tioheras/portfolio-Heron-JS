391df8e287c3809aed5ff77ce15307b50f9f30e9
tioheras <eng.heroncarli@gmail.com>
2024-07-25 15:26:15 -0300

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,14 @@
 
+function updateProfileInfo(profileData) {
+
+    const photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
+
+}
+
 (async () => {
 
     const profileData = await fetchProfileData()
-    console.log(profileData)
+    updateProfiledate
 })()
